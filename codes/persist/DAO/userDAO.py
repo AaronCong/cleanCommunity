da3f66603cb2a8e38e1b221d6d5dc77612b40468
AaronCong <congxu0221@126.com>
2019-07-13 20:12:37 +0800

    push

--- codes/persist/DAO/userDAO.py
+++ codes/persist/DAO/userDAO.py
@@ -3,4 +3,6 @@ from codes.persist.models.User import User
 def searchDumper(user):
     userLon, userLat = user.apt_lon, user.apt_lat
     cleanerList = User.query.filter(User.status == 2).all()
+    distance = []
     for cleaner in cleanerList:
+        distance.append()
